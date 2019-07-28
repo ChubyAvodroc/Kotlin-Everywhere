7be85c114f0ef36855224fd47d2b8c049e3fb205
David Córdova Cardoza <isc.ipn.david@gmail.com>
2019-07-28 13:41:40 -0500

    Creating Java backend project
    New project with Spring Boot

---
+++ KEBackendJava/src/main/java/dev/chuby/ke_backend_java/KeBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.chuby.ke_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeBackendJavaApplication.class, args);
+	}
+
+}
---
+++ KEBackendJava/src/test/java/dev/chuby/ke_backend_java/KeBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chuby.ke_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
