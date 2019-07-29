859caec1012912675016bcc3f61891e41c133234
David Córdova Cardoza <isc.ipn.david@gmail.com>
2019-07-29 13:56:36 -0500

    Adding button click listeners
    When action buttons are clicked then actual navigation to corresponding screen is performed

--- KEAndroidApp/app/src/main/java/dev/chuby/ke_android_app/ActionsFragment.kt
+++ KEAndroidApp/app/src/main/java/dev/chuby/ke_android_app/ActionsFragment.kt
@@ -4,12 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
 class ActionsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_actions, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnAddAttendee = view.findViewById<Button>(R.id.btn_add_attendee)
+        val btnShowAttendees = view.findViewById<Button>(R.id.btn_show_attendees)
+
+        btnAddAttendee.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_actionsFragment_to_addAttendeeFragment)
+        )
+
+        btnShowAttendees.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_actionsFragment_to_attendeesFragment)
+        )
     }
 
 }
