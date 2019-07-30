2536d4faae1ffed3a15fb6985846cdf568b57315
David Córdova Cardoza <isc.ipn.david@gmail.com>
2019-07-29 23:04:44 -0500

    Attaching viewmodel with Attendees screen
    The view model will ask for available attendees, based on the state result the view will show either the list of attendees or an snackbar error

--- KEAndroidApp/app/src/main/java/dev/chuby/ke_android_app/ui/AttendeesFragment.kt
+++ KEAndroidApp/app/src/main/java/dev/chuby/ke_android_app/ui/AttendeesFragment.kt
@@ -1,16 +1,82 @@
 package dev.chuby.ke_android_app.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import dev.chuby.ke_android_app.AttendeesRepository
 import dev.chuby.ke_android_app.R
+import dev.chuby.ke_android_app.model.Attendee
+import dev.chuby.ke_android_app.model.Failure
+import dev.chuby.ke_android_app.model.Loading
+import dev.chuby.ke_android_app.model.Success
+import dev.chuby.ke_android_app.viewmodel.AttendeeViewModelFactory
+import dev.chuby.ke_android_app.viewmodel.AttendeesViewModel
 
 class AttendeesFragment : Fragment() {
 
+    companion object {
+        private const val TAG = "AttendeesFragment"
+    }
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(
+            this,
+            AttendeeViewModelFactory(attendeesRepository)
+        )[AttendeesViewModel::class.java]
+    }
+
+    private lateinit var rvAttendees: RecyclerView
+    private lateinit var pbLoader: ProgressBar
+    private val adapter = AttendeeAdapter()
+
+    val attendeesRepository: AttendeesRepository? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_attendees, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rvAttendees = view.findViewById(R.id.rv_attendees)
+        pbLoader = view.findViewById(R.id.pb_loader)
+
+        rvAttendees.adapter = adapter
+
+        when (val viewState = viewModel.loadAttendees()) {
+            is Loading -> showLoader()
+            is Success -> showAttendees(viewState.data)
+            is Failure -> showFailureMessage(viewState.message)
+        }
+
+    }
+
+    private fun showAttendees(attendees: List<Attendee>) {
+        hideLoader()
+        adapter.submitList(attendees)
+        Log.i(TAG, "List of attendees $attendees")
+    }
+
+    private fun showFailureMessage(message: String) {
+        hideLoader()
+        Log.e(TAG, "There was an error $message")
+        Snackbar.make(view!!, message, Snackbar.LENGTH_LONG).show()
+
+    }
+
+    private fun showLoader() {
+        pbLoader.visibility = View.VISIBLE
+    }
+
+    private fun hideLoader() {
+        pbLoader.visibility = View.GONE
     }
 
 }
