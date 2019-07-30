aefd21c4cd35385882d169c7bc773db8f2b5d114
David Córdova Cardoza <isc.ipn.david@gmail.com>
2019-07-30 12:02:42 -0500

    Loading avatar image
    Attendee ViewModel now loads and displays the avatar image when attendees list is showed

--- KEAndroidApp/app/src/main/java/dev/chuby/ke_android_app/ui/AttendeeViewHolder.kt
+++ KEAndroidApp/app/src/main/java/dev/chuby/ke_android_app/ui/AttendeeViewHolder.kt
@@ -1,8 +1,10 @@
 package dev.chuby.ke_android_app.ui
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import dev.chuby.ke_android_app.R
 import dev.chuby.ke_android_app.model.Attendee
 
@@ -10,11 +12,13 @@ class AttendeeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val tvName: TextView = itemView.findViewById(R.id.tv_name)
     private val tvLastName: TextView = itemView.findViewById(R.id.tv_last_name)
+    private val ivAvatar: ImageView = itemView.findViewById(R.id.iv_avatar)
 
     fun bindTo(attendee: Attendee) {
         with(attendee) {
             tvName.text = name
             tvLastName.text = lastName
+            Picasso.get().load(avatar).into(ivAvatar)
         }
     }
 }
