bdb872726376b535b8abf24e45f66e4b4e51c5f9
David Córdova Cardoza <isc.ipn.david@gmail.com>
2019-08-01 13:53:37 -0500

    Setting default Repository to ViewModel factory
    Using previously KtorAttendeeRepository object class

--- KEAndroidApp/app/src/main/java/dev/chuby/ke_android_app/viewmodel/AttendeeViewModelFactory.kt
+++ KEAndroidApp/app/src/main/java/dev/chuby/ke_android_app/viewmodel/AttendeeViewModelFactory.kt
@@ -3,8 +3,9 @@ package dev.chuby.ke_android_app.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import dev.chuby.ke_android_app.AttendeesRepository
+import dev.chuby.ke_android_app.KtorAttendeesRepository
 
-class AttendeeViewModelFactory(private val attendeesRepository: AttendeesRepository? = null) : ViewModelProvider.Factory {
+class AttendeeViewModelFactory(private val attendeesRepository: AttendeesRepository = KtorAttendeesRepository) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(AttendeesViewModel::class.java))
