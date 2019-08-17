377a237a9538c4481d5940c0c0ef3e5e6445ef7d
David Córdova Cardoza <isc.ipn.david@gmail.com>
2019-08-16 22:52:25 -0500

    Refactoring AttendeesRepository to return Resource instead of specific type
    This version does not handle errors or different status codes, that will come in a future commit

--- KEAndroidApp/app/src/main/java/dev/chuby/ke_android_app/KtorAttendeesRepository.kt
+++ KEAndroidApp/app/src/main/java/dev/chuby/ke_android_app/KtorAttendeesRepository.kt
@@ -1,6 +1,8 @@
 package dev.chuby.ke_android_app
 
 import dev.chuby.ke_android_app.model.Attendee
+import dev.chuby.ke_android_app.model.DataResource
+import dev.chuby.ke_android_app.model.Resource
 import io.ktor.client.HttpClient
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
@@ -8,6 +10,7 @@ import io.ktor.client.features.logging.LogLevel
 import io.ktor.client.features.logging.Logger
 import io.ktor.client.features.logging.Logging
 import io.ktor.client.features.logging.SIMPLE
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.post
@@ -35,21 +38,28 @@ object KtorAttendeesRepository : AttendeesRepository {
 
     }
 
-    override suspend fun createAttendee(attendee: Attendee): Attendee = withContext(Dispatchers.IO) {
+    override suspend fun getAttendees(): Resource<List<Attendee>> = withContext(Dispatchers.IO) {
+        val attendees = client.get<List<Attendee>>("$BASE_URL/attendees")
+        DataResource(attendees)
+    }
+
+    override suspend fun getAttendee(id: Long): Resource<Attendee> = withContext(Dispatchers.IO) {
+        val attendee = client.get<Attendee>("$BASE_URL/attendees/$id")
+        DataResource(attendee)
+    }
+
+    override suspend fun createAttendee(attendee: Attendee): Resource<Attendee> = withContext(Dispatchers.IO) {
         val savedAttendee = client.post<Attendee>("$BASE_URL/attendees") {
             header(HttpHeaders.ContentType, "application/json")
             body = attendee
         }
-        savedAttendee
+        DataResource(savedAttendee)
     }
 
-    override suspend fun getAttendees(): List<Attendee> = withContext(Dispatchers.IO) {
-        val attendees = client.get<List<Attendee>>("$BASE_URL/attendees")
-        attendees
-    }
-
-    override suspend fun getAttendee(id: Long): Attendee = withContext(Dispatchers.IO) {
-        val attendee = client.get<Attendee>("$BASE_URL/attendees/$id")
-        attendee
+    override suspend fun removeAttendee(id: Long): Resource<Attendee> = withContext(Dispatchers.IO) {
+        val deletedAttendee = client.delete<Attendee>("$BASE_URL/attendees/$id") {
+            header(HttpHeaders.ContentType, "aaplication/json")
+        }
+        DataResource(deletedAttendee)
     }
 }
